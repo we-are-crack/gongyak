8e3ebae4e4053f192f8a79f9f03904c79caa7e62
on1ystar <tjdwls0607@naver.com>
2025-05-06 17:17:10 +0900

    init

---
+++ Web-Server/gongyak/src/main/java/wac/gongyak/GongyakApplication.java
@@ -0,0 +1,13 @@
+package wac.gongyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongyakApplication.class, args);
+	}
+
+}
---
+++ Web-Server/gongyak/src/test/java/wac/gongyak/GongyakApplicationTests.java
@@ -0,0 +1,13 @@
+package wac.gongyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongyakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
