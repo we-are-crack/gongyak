dd213e572d8820890dcb818d46b217051b528631
on1ystar <tjdwls0607@naver.com>
2025-05-17 17:39:31 +0900

    중복 입력 방비/글자수 제한/코드 가독성 개선

--- Web-Server/src/homeController.js
+++ Web-Server/src/homeController.js
@@ -13,7 +13,17 @@ export const home = (req, res) => {
 
 export const pledges = async (req, res) => {
   // 클라이언트에서 전달된 검색어 추출
-  const searchQuery = req.query.q || '';
+  let searchQuery = req.query.q || '';
+
+  // 검색어 길이 제한 검사
+  if (isSearchQueryTooLong(searchQuery)) {
+    return res.status(200).set('Content-Type', 'application/json; charset=utf-8').json({
+      search: '',
+      status: 'tooLong',
+      htmlData: '',
+    });
+  }
+
   const url = `http://127.0.0.1:5000/query?q=${encodeURIComponent(searchQuery)}`;
   const headers = { Accept: 'application/json' };
 
@@ -62,3 +72,12 @@ export const pledges = async (req, res) => {
     });
   }
 };
+
+// 검색어 길이 검사 함수
+function isSearchQueryTooLong(query, maxLength = 50) {
+  if (typeof query === 'string' && query.length > maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+}
