2290d8f083f4956984b972ce9ea0bcfe7e0ac99c
jongseong <eello.bdev@gmail.com>
2025-05-26 16:58:54 +0900

    Fix : 응답 형식 변환: html -> json

--- llm-flask/app/routes/query.py
+++ llm-flask/app/routes/query.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, Response
 import os
 from werkzeug.utils import secure_filename
+import json
 
 from app.service.query_service import query, is_relevant_question
 
@@ -13,8 +14,10 @@ def query_to_llm():
     print(f"질문: {q}")
 
     if not is_relevant_question(q):
-        return jsonify({"search": "", "status": "invalid", "htmlData": ""}), 200
+        return jsonify({"searchQuery": "", "status": "invalid", "data": ""}), 200
 
-    answer = query(q)
+    result = query(q).strip().removeprefix("```json").removesuffix("```").strip()
+    data = json.loads(result)
+    print(data)
 
-    return jsonify({"search": q, "status": "ok", "htmlData": answer}), 200
+    return jsonify({"searchQuery": q, "status": "ok", "data": data}), 200
